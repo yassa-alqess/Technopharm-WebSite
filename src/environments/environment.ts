778cf249196a663a5bf7497e4df63fbb1d82dd12
Ahmed Khairy <=>
2023-06-25 13:34:39 +0300

    change API path and add custom condition

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     defaultLang: 'en',
     hotLine: 19955,
     email: 'INFO@delmar-attalla.com',
-    HOST_API: 'https://delmar-test.linkedgates.com/', // Production API URL
+    HOST_API: 'http://delmar-test.linkedgates.com/', // Production API URL
     mobileAppURL: "https://play.google.com/store/apps/details?id=com.LinkedGates.DelmarAttalla&hl=en&gl=US",
     magazineURL: "https://online.fliphtml5.com/gjkwf/bzjf/#p=1",
     mobileAppDiscountPercentage: 50,
