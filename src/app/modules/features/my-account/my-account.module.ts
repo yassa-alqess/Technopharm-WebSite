d75cfb6d1222da5c56b1d5134b93d1e4b51e36a5
Ahmed Khairy <=>
2023-07-09 00:34:17 +0300

    implement my-account tabs

--- src/app/modules/features/my-account/my-account.module.ts
+++ src/app/modules/features/my-account/my-account.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { MyAccountRoutingModule } from './my-account-routing.module';
 import { MyAccountComponent } from './pages/my-account/my-account.component';
+import { BaseSharedModule } from 'shared/sub-modules/base-shared';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { MyAccountComponent } from './pages/my-account/my-account.component';
   ],
   imports: [
     CommonModule,
+    MatTabsModule,
+    BaseSharedModule,
     MyAccountRoutingModule
   ]
 })
--- src/app/modules/features/my-account/pages/my-account/my-account.component.ts
+++ src/app/modules/features/my-account/pages/my-account/my-account.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'del-my-account',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-account.component.scss']
 })
 export class MyAccountComponent {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
 
+  myAccountTabs = [
+    {
+      tabName: 'MY_ACCOUNT.MY_INFO',
+      tabKey: 'MY_INFO'
+    },
+    {
+      tabName: 'MY_ACCOUNT.ORDERS',
+      tabKey: 'ORDERS'
+    },
+    {
+      tabName: 'MY_ACCOUNT.ADDRESSES',
+      tabKey: 'ADDRESSES'
+    },
+    {
+      tabName: 'MY_ACCOUNT.WALLET',
+      tabKey: 'WALLET'
+    },
+  ];
+  activeTabKey = this.myAccountTabs[0].tabKey;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const tabKey = this.activatedRoute.snapshot.queryParamMap.get('tabKey') || '';
+    this.getActiveTab(tabKey);
+  }
+
+  getActiveTab(tabKey: string) {
+    this.activeTabKey = tabKey;
+    this.router.navigate(['.'], { relativeTo: this.activatedRoute, queryParams: { tabKey } });
+  }
 }
