cdbc837908ebcac190a4b4320de740f3b8a0b3cc
Ahmed Khairy <=>
2023-07-08 15:07:07 +0300

    change standalone import

--- src/app/modules/shared/components/index.ts
+++ src/app/modules/shared/components/index.ts
@@ -1,1 +1,13 @@
-export * from './standalone';
+export * from './standalone/breadcrumb/breadcrumb.component';
+export * from './standalone/category/category.component';
+export * from './standalone/category-sidebar/category-sidebar.component';
+export * from './standalone/product/product.component';
+export * from './standalone/product-details/product-details.component';
+export * from './standalone/title/title.component';
+export * from './standalone/sub-title/sub-title.component';
+export * from './standalone/button/button.component';
+
+export * from './standalone/input-text/input-text.component';
+export * from './standalone/datepicker/datepicker.component';
+export * from './standalone/dropdown/dropdown.component';
+export * from './standalone/radio-button/radio-button.component';
